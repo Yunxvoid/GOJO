d7fca37f87e18ebeb54dbfb1797bed5f5afe3952
YUN神 <110736685+Yunxvoid@users.noreply.github.com>
2022-11-09 08:24:29 -0800

    Create send.py

---
+++ GOJO/modules/send.py
@@ -0,0 +1,24 @@
+from GOJO import dispatcher
+from GOJO.modules.disable import DisableAbleCommandHandler
+
+from GOJO.modules.helper_funcs.alternate import send_message
+
+
+def send(update, context):
+	args = update.effective_message.text.split(None, 1)
+	creply = args[1]
+	send_message(update.effective_message, creply)
+
+__help__ = """The Send Module Allows you to send a custom message to users in a chat
+`/snd` :Send the given message
+Note - /snd Hi will send the message hi to the chat"""
+
+__mod_name__ = "Sᴇɴᴅ"
+
+
+ADD_CCHAT_HANDLER = DisableAbleCommandHandler("snd", send, run_async = True)
+dispatcher.add_handler(ADD_CCHAT_HANDLER)
+__command_list__ = ["snd"]
+__handlers__ = [
+    ADD_CCHAT_HANDLER
+]
