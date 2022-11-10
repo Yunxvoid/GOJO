824ef52edbb8fa7224031484a9f4f40faeedd717
YUN神 <110736685+Yunxvoid@users.noreply.github.com>
2022-11-10 03:18:11 -0800

    Create alive.py

---
+++ GOJO/modules/alive.py
@@ -0,0 +1,61 @@
+from GOJO.vars import ALIVE_MEDIA, UPDATE_CHANNEL, SUPPORT_CHAT, OWNER_USERNAME, NETWORK, NETWORK_USERNAME
+from GOJO import dispatcher
+from GOJO.modules.disable import DisableAbleCommandHandler
+from telegram import ParseMode, Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CallbackContext
+
+bot_name = f"{dispatcher.bot.first_name}"
+
+ALIVE_ID = ALIVE_MEDIA.split(".")
+alive_id = ALIVE_ID[-1]
+
+def awake(update: Update, context: CallbackContext):
+    message = update.effective_message
+    buttons = [
+        [
+        InlineKeyboardButton(
+            text="Updates",
+            url=f"https://t.me/{UPDATE_CHANNEL}"),
+        InlineKeyboardButton(
+            text="Support",
+            url=f"https://t.me/{SUPPORT_CHAT}"),
+        ],
+     ]
+    
+    first_name = update.effective_user.first_name
+    user = message.from_user
+
+    TEXT = f"""
+    <b>Hi <a href="tg://user?id={user.id}">{first_name}</a>, I'm {bot_name} Robot.
+ I'm Working Properly
+ My Owner : <a href="https://t.me/{OWNER_NAME}">{OWNER_USERNAME}</a></b>
+    """
+    
+    else:
+        TEXT = TEXT + "\n<b>Thanks For Adding Me Here </b>"
+
+    try:
+        if alive_id in ("jpeg", "jpg", "png"):
+            message.reply_photo(ALIVE_MEDIA, caption=TEXT, reply_markup=InlineKeyboardMarkup(buttons),parse_mode=ParseMode.HTML)
+        elif alive_id in ("mp4", "mkv"):
+            message.reply_video(ALIVE_MEDIA, caption=TEXT, reply_markup=InlineKeyboardMarkup(buttons),parse_mode=ParseMode.HTML)
+        elif alive_id in ("gif", "webp"):
+            message.reply_animation(ALIVE_MEDIA, caption=TEXT, reply_markup=InlineKeyboardMarkup(buttons),parse_mode=ParseMode.HTML)
+        else:
+            message.reply_text(TEXT, reply_markup=InlineKeyboardMarkup(buttons),parse_mode=ParseMode.HTML)
+
+    except:
+        message.reply_text(TEXT, reply_markup=InlineKeyboardMarkup(buttons),parse_mode=ParseMode.HTML)
+
+ALIVE_HANDLER = DisableAbleCommandHandler("alive", awake, run_async=True)
+dispatcher.add_handler(ALIVE_HANDLER)
+__command_list__ = ["alive"]
+__handlers__ = [
+    ALIVE_HANDLER,
+]
+
+__mod_name__ = "ᴀʟɪᴠᴇ"
+__help__ = """
+*ALIVE*
+ ❍ `/alive` :Check BOT status
+"""
