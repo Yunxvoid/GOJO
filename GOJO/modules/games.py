c928996a3c4ec76d0ce8eb186489e11559a7e0b0
YUN神 <110736685+Yunxvoid@users.noreply.github.com>
2022-11-10 01:50:42 -0800

    Create games.py

---
+++ GOJO/modules/games.py
@@ -0,0 +1,41 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from Shikimori import pbot as app
+
+
+
+__mod_name__ = "ɢᴀᴍᴇs"
+__help__ = """
+Use these commands and try to score.\n\n
+ ❍ `/dice` - Dice 🎲\n
+ ❍ `/dart` - Dart 🎯\n
+ ❍ `/basket` - Basket Ball 🏀\n
+ ❍ `/bowling` - Bowling Ball 🎳\n
+ ❍ `/football` - Football ⚽\n
+ ❍ `/slot` - Spin slot machine 🎰
+"""
+
+@app.on_message(filters.command("dice"))
+async def throw_dice(client, message: Message): 
+    await client.send_dice(message.chat.id, "🎲")
+
+@app.on_message(filters.command("dart"))
+async def throw_dice(client, message: Message): 
+    await client.send_dice(message.chat.id, "🎯")
+
+@app.on_message(filters.command("basket"))
+async def throw_dice(client, message: Message): 
+    await client.send_dice(message.chat.id, "🏀")
+
+@app.on_message(filters.command("bowling"))
+async def throw_dice(client, message: Message): 
+    await client.send_dice(message.chat.id, "🎳")
+
+@app.on_message(filters.command("football"))
+async def throw_dice(client, message: Message): 
+    await client.send_dice(message.chat.id, "⚽")
+
+@app.on_message(filters.command("slot"))
+async def throw_dice(client, message: Message): 
+    await client.send_dice(message.chat.id, "🎰")
